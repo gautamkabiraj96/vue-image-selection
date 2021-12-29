cac9610d2882109ff3c2f870c4e7f7a1a148b6cf
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2021-12-30 01:27:40 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <SelectImage />
+  </div>
+</template>
+
+<script>
+import SelectImage from "./components/SelectImage";
+
+export default {
+  name: "App",
+  components: {
+    SelectImage,
+  },
+};
+</script>
